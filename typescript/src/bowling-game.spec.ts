9b8070ec6f2f1f82b6859c08f8c7ab8b7be9bfb2
Paolo Sciarra <itbhps@gmail.com>
2025-02-14 18:57:55 +0100

    now we have the concept of Frame

--- typescript/src/bowling-game.spec.ts
+++ typescript/src/bowling-game.spec.ts
@@ -7,13 +7,18 @@ describe("bowling game", () => {
     });
 
     it("scores n < 10 pin in a roll", () => {
+        expect(game.frame()).toBe(1);
         game.roll(4);
+        expect(game.frame()).toBe(1);
         expect(game.score()).toBe(4);
     });
 
     it("scores n < 10 pin in two rolls", () => {
+        expect(game.frame()).toBe(1);
         game.roll(4);
+        expect(game.frame()).toBe(1);
         game.roll(5);
+        expect(game.frame()).toBe(2);
         expect(game.score()).toBe(9);
     });
 });
--- typescript/src/bowling-game.ts
+++ typescript/src/bowling-game.ts
@@ -1,15 +1,53 @@
 export class Game {
-    private rolls: number[];
+
+    private frames: Frame[];
+    private currentFrame: Frame;
 
     constructor() {
-        this.rolls = [];
+        this.currentFrame = new GenericFrame();
+        this.frames = [];
+    }
+    score(): number {
+        return this.frames.length == 0
+            ? this.currentFrame.score()
+            : this.frames.map(it => it.score()).reduce(addNumbers);
     }
 
-    score(): number {
-        return this.rolls.reduce((prev: number, curr: number) => prev + curr);
+    roll(n: number): void {
+        this.currentFrame.roll(n);
+        if (this.currentFrame.isCompleted()) {
+            this.frames.push(this.currentFrame);
+            this.currentFrame = new GenericFrame();
+        }
+    };
+
+    frame(): number {
+        return this.frames.length + 1;
     }
-    
+}
+
+interface Frame {
+    score(): number;
+    roll(n: number): unknown;
+    isCompleted(): boolean
+}
+
+class GenericFrame implements Frame {
+    private rolls: number[]
+
+    constructor() {
+        this.rolls = []
+    }
+    score(): number {
+        return this.rolls.reduce(addNumbers);
+    }
+    isCompleted(): boolean {
+        return this.rolls.length >= 2 || this.score() >= 10
+    }
+
     roll(n: number): void {
         this.rolls.push(n);
-    };
+    }
 }
+
+const addNumbers = (prev: number, curr: number) => prev + curr;
