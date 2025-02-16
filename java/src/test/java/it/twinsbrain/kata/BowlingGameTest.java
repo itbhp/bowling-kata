4dc55931db155b3b935e4b061631ad9be00d216e
Paolo Sciarra <itbhps@gmail.com>
2025-02-16 16:04:55 +0100

    test refactoring

--- java/src/test/java/it/twinsbrain/kata/BowlingGameTest.java
+++ java/src/test/java/it/twinsbrain/kata/BowlingGameTest.java
@@ -2,6 +2,8 @@ package it.twinsbrain.kata;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
@@ -22,10 +24,11 @@ public class BowlingGameTest {
     assertThat(game.score(), equalTo(0));
   }
 
-  @Test
-  void one_pin_down_one_score() {
-    game.roll(1);
+  @ParameterizedTest
+  @ValueSource(ints = {1,2,3,4,5,6,7,8,9,10})
+  void one_or_ten_pins_down_same_score(int pins) {
+    game.roll(pins);
 
-    assertThat(game.score(), equalTo(1));
+    assertThat(game.score(), equalTo(pins));
   }
 }
