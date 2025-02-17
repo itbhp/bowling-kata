03b4f23fe317b3481ce3e99545aacbee04406367
Paolo Sciarra <itbhps@gmail.com>
2025-02-17 20:56:06 +0100

    stored list of pins rolled down

--- java/src/main/java/it/twinsbrain/kata/Game.java
+++ java/src/main/java/it/twinsbrain/kata/Game.java
@@ -1,14 +1,24 @@
 package it.twinsbrain.kata;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Game {
 
-  private int pins;
+  private final List<Integer> pins = new ArrayList<>();
 
   public void roll(int pins) {
-    this.pins += pins;
+    this.pins.add(pins);
   }
 
   public int score() {
-    return pins;
+    var score = 0;
+    for (int i = 0; i < pins.size(); i = i + 2) {
+      score += pins.get(i);
+      if (i + 1 < pins.size()) {
+        score += pins.get(i+1);
+      }
+    }
+    return score;
   }
 }
--- java/src/test/java/it/twinsbrain/kata/BowlingGameTest.java
+++ java/src/test/java/it/twinsbrain/kata/BowlingGameTest.java
@@ -9,7 +9,7 @@ import org.junit.jupiter.params.provider.ValueSource;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 
-public class BowlingGameTest {
+class BowlingGameTest {
 
   private Game game;
 
