a76bd561349accefac457101463e43ef66cdd42a
Paolo Sciarra <itbhps@gmail.com>
2025-02-22 16:03:32 +0100

    refactor tests + add test spare on the last frame that doesn't work (to fix)

--- java/src/test/java/it/twinsbrain/kata/BowlingGameTest.java
+++ java/src/test/java/it/twinsbrain/kata/BowlingGameTest.java
@@ -3,14 +3,18 @@ package it.twinsbrain.kata;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 
+import java.util.Arrays;
+import java.util.function.Consumer;
+import java.util.stream.IntStream;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
-import java.util.Arrays;
-
 class BowlingGameTest {
+
+  private static final Consumer<Game> ZERO_PINS = pinsDown(0);
 
   private Game game;
 
@@ -22,12 +26,11 @@ class BowlingGameTest {
   @Test
   void no_pin_down_zero_score() {
     game.roll(0);
-
     assertThat(game.score(), equalTo(0));
   }
 
   @ParameterizedTest
-  @ValueSource(ints = {1,2,3,4,5,6,7,8,9,10})
+  @ValueSource(ints = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10})
   void one_or_ten_pins_down_same_score(int pins) {
     roll(pins);
     assertThat(game.score(), equalTo(pins));
@@ -38,6 +41,19 @@ class BowlingGameTest {
     roll(4, 5);
 
     assertThat(game.score(), equalTo(9));
+  }
+
+  @Test
+  void worste_game() {
+    roll(20, ZERO_PINS);
+    assertThat(game.score(), equalTo(0));
+  }
+
+  @Test
+  void one_pin_then_zero() {
+    roll(1);
+    roll(19, ZERO_PINS);
+    assertThat(game.score(), equalTo(1));
   }
 
   @Test
@@ -54,7 +70,23 @@ class BowlingGameTest {
     assertThat(game.score(), equalTo(14 + 9)); // 23
   }
 
-  private void roll(int...rolledPins) {
+  @Disabled
+  void spare_on_the_last_frame() {
+    roll(18, pinsDown(4)); // 4 * 18 = 72
+    roll(4, 6, 4);
+
+    assertThat(game.score(), equalTo(4 * 18 + 14 )); // 86
+  }
+
+  private static Consumer<Game> pinsDown(int pins) {
+    return game -> game.roll(pins);
+  }
+
+  private void roll(int... rolledPins) {
     Arrays.stream(rolledPins).forEach(pins -> game.roll(pins));
+  }
+
+  private void roll(int times, Consumer<Game> play) {
+    IntStream.range(0, times - 1).forEach(_ -> play.accept(game));
   }
 }
